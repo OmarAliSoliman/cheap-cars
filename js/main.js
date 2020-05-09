7e3904562d50625d64cda51bf6f64d81dd8f5f40
omar <omarasmar9955@gmail.com>
2020-05-09 14:49:34 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+  AOS.init();
+
   var swiper = new Swiper(".swiper-ads", {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -22,4 +25,43 @@ $(document).ready(function () {
       },
     },
   });
+
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop: true,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop:true,
+    loopedSlides: 5, //looped slides should be the same
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  });
+
 });
+
+var btnClose = document.querySelector('#btnclose');
+var searchBox = document.querySelector('.search-box');
+var open = true;
+btnClose.addEventListener('click', ()=>{
+  if(open){
+    searchBox.style.display = "none";
+    searchBox.style.transition = "all 1s ease";
+    btnClose.innerHTML = "<span> <i class='fas fa-chevron-down'></i> بحث متقدم</span>"
+  }else{
+    searchBox.style.display = "block";
+    searchBox.style.transition = "all 1s ease";
+    btnClose.innerHTML = "<span> <i class='fas fa-chevron-up'></i> بحث متقدم</span>"
+  }
+  open= !open;
+})
